b992797ad2057ffda307eecc073e916664634ae7
lacondel <ilya.gaffarov@yandex.ru>
2025-05-22 13:47:23 +0300

    Added models

---
+++ backend/models/Post.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    tags: [{
+        type: String
+    }],
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    comments: [{
+        text: String,
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }]
+});
+
+module.exports = mongoose.model('Post', postSchema);
